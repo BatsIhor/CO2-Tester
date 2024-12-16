a51407ed6efb2a691c41e490bf593641a1ed3fb5
Ihor Bats <batsihor@gmail.com>
2024-12-16 14:32:10 -0500

    Working copy, shows CO2 level

--- src/GxEPD_Example.ino
+++ src/GxEPD_Example.ino
@@ -1,18 +1,5 @@
-/*
-    LilyGo Ink Screen Series Test
-        - Created by Lewis he
-*/
-
-// According to the board, cancel the corresponding macro definition
- #define LILYGO_T5_V213
- //#define LILYGO_EPD_DISPLAY
-// #define LILYGO_T5_V22
-// #define LILYGO_T5_V24
-// #define LILYGO_T5_V28
-// #define LILYGO_T5_V102                           
-// #define LILYGO_T5_V266
-// #define LILYGO_EPD_DISPLAY         //T-Display 1.02 inch epaper   //Depend  https://github.com/adafruit/Adafruit_NeoPixel
-// #define LILYGO_EPD_DISPLAY_154
+#define LILYGO_T5_V213
+#define LILYGO_EPD_DISPLAY
 
 #include <boards.h>
 #include <GxEPD.h>
@@ -20,38 +7,15 @@
 #include <FS.h>
 #include <Wire.h>
 #include <SensirionI2CScd4x.h>
-#include <GxGDEH0213B72/GxGDEH0213B72.h> // Include the display driver for your specific display
+// #include <GxGDEH0213B72/GxGDEH0213B72.h> // Include the display driver for your specific display
 #include <Fonts/FreeMonoBold9pt7b.h> // Include a font
 
-#if defined(LILYGO_T5_V102) || defined(LILYGO_EPD_DISPLAY)
-#include <GxGDGDEW0102T4/GxGDGDEW0102T4.h> //1.02" b/w
-#include <Adafruit_NeoPixel.h>             //Depend  https://github.com/adafruit/Adafruit_NeoPixel
-#elif defined(LILYGO_T5_V266)
-#include <GxDEPG0266BN/GxDEPG0266BN.h>    // 2.66" b/w   form DKE GROUP
-#elif defined(LILYGO_T5_V213)
-#include <GxDEPG0213BN/GxDEPG0213BN.h>    // 2.13" b/w  form DKE GROUP
-#else
-// #include <GxGDGDEW0102T4/GxGDGDEW0102T4.h> //1.02" b/w
-// #include <GxGDEW0154Z04/GxGDEW0154Z04.h>  // 1.54" b/w/r 200x200
-// #include <GxGDEW0154Z17/GxGDEW0154Z17.h>  // 1.54" b/w/r 152x152
-// #include <GxGDEH0154D67/GxGDEH0154D67.h>  // 1.54" b/w
-// #include <GxDEPG0150BN/GxDEPG0150BN.h>    // 1.51" b/w   form DKE GROUP
-// #include <GxDEPG0266BN/GxDEPG0266BN.h>    // 2.66" b/w   form DKE GROUP
-// #include <GxDEPG0290R/GxDEPG0290R.h>      // 2.9" b/w/r  form DKE GROUP
-// #include <GxDEPG0290B/GxDEPG0290B.h>      // 2.9" b/w    form DKE GROUP
-// #include <GxGDEW029Z10/GxGDEW029Z10.h>    // 2.9" b/w/r  form GoodDisplay
-// #include <GxGDEW0213Z16/GxGDEW0213Z16.h>  // 2.13" b/w/r form GoodDisplay
 // #include <GxGDE0213B1/GxGDE0213B1.h>      // 2.13" b/w  old panel , form GoodDisplay
-// #include <GxGDEH0213B72/GxGDEH0213B72.h>  // 2.13" b/w  old panel , form GoodDisplay
-// #include <GxGDEH0213B73/GxGDEH0213B73.h>  // 2.13" b/w  old panel , form GoodDisplay
-// #include <GxGDEM0213B74/GxGDEM0213B74.h>  // 2.13" b/w  form GoodDisplay 4-color
-// #include <GxGDEW0213M21/GxGDEW0213M21.h>  // 2.13"  b/w Ultra wide temperature , form GoodDisplay
-// #include <GxDEPG0213BN/GxDEPG0213BN.h>    // 2.13" b/w  form DKE GROUP
-// #include <GxGDEW027W3/GxGDEW027W3.h>      // 2.7" b/w   form GoodDisplay
-// #include <GxGDEW027C44/GxGDEW027C44.h>    // 2.7" b/w/r form GoodDisplay
-// #include <GxGDEH029A1/GxGDEH029A1.h>      // 2.9" b/w   form GoodDisplay
-// #include <GxDEPG0750BN/GxDEPG0750BN.h>    // 7.5" b/w   form DKE GROUP
-#endif
+//  #include <GxGDEH0213B72/GxGDEH0213B72.h>  // 2.13" b/w  old panel , form GoodDisplay
+//  #include <GxGDEH0213B73/GxGDEH0213B73.h>  // 2.13" b/w  old panel , form GoodDisplay
+//  #include <GxGDEM0213B74/GxGDEM0213B74.h>  // 2.13" b/w  form GoodDisplay 4-color
+//  #include <GxGDEW0213M21/GxGDEW0213M21.h>  // 2.13"  b/w Ultra wide temperature , form GoodDisplay
+#include <GxDEPG0213BN/GxDEPG0213BN.h> // 2.13" b/w  form DKE GROUP
 
 #include GxEPD_BitmapExamples
 
@@ -68,133 +32,74 @@
 SensirionI2CScd4x scd4x;
 
 // Initialize the display
-GxIO_Class io(SPI, /*CS=*/ 5, /*DC=*/ 17, /*RST=*/ 16); // Adjust pins as needed
-GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4); // Adjust pins as needed
-
-#if defined(_GxGDEW0213Z16_H_) || defined(_GxGDEW029Z10_H_) || defined(_GxGDEW027C44_H_) ||defined(_GxGDEW0154Z17_H_) || defined(_GxGDEW0154Z04_H_) || defined(_GxDEPG0290R_H_)
-#define _HAS_COLOR_
-#endif
-
-#if defined(LILYGO_EPD_DISPLAY)
-Adafruit_NeoPixel strip(RGB_STRIP_COUNT, RGB_STRIP_PIN, NEO_GRBW + NEO_KHZ800);
-#endif /*LILYGO_EPD_DISPLAY_102*/
-
+GxIO_Class io(SPI, /*CS=*/5, /*DC=*/17, /*RST=*/16); // Adjust pins as needed
+GxEPD_Class display(io, /*RST=*/16, /*BUSY=*/4);     // Adjust pins as needed
 
 void showFont(const char name[], const GFXfont *f);
 void drawCornerTest(void);
 
 void setup()
 {
-    Wire.begin();
     bool rlst = false;
     Serial.begin(115200);
     Serial.println();
     Serial.println("setup");
 
-#if defined(LILYGO_EPD_DISPLAY)
-    pinMode(EPD_POWER_ENABLE, OUTPUT);
-    digitalWrite(EPD_POWER_ENABLE, HIGH);
-    delay(50);
-    // strip test
-    strip.begin();
-    strip.show();
-    strip.setBrightness(200);
-    int i = 0;
-    while (i < 5) {
-        uint32_t color[] = {0xFF0000, 0x00FF00, 0x0000FF, 0x000000};
-        strip.setPixelColor(0, color[i]);
-        strip.show();
-        delay(1000);
-        i++;
-    }
-    strip.setPixelColor(0, 0);
-    strip.show();
-#endif /*LILYGO_EPD_DISPLAY*/
-    
-#if defined(LILYGO_EPD_DISPLAY_102)
-    pinMode(EPD_POWER_ENABLE, OUTPUT);
-    digitalWrite(EPD_POWER_ENABLE, HIGH);
-#endif /*LILYGO_EPD_DISPLAY_102*/
-#if defined(LILYGO_T5_V102)
-    pinMode(POWER_ENABLE, OUTPUT);
-    digitalWrite(POWER_ENABLE, HIGH);
-#endif /*LILYGO_T5_V102*/
-  
+    // Initialize the SCD4x sensor
+#define SDA_PIN 33
+#define SCL_PIN 32
+    Wire.begin(SDA_PIN, SCL_PIN);
+    uint16_t error;
+    char errorMessage[256];
 
+    scd4x.begin(Wire);
+    error = scd4x.stopPeriodicMeasurement();
+    if (error)
+    {
+        Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+        // errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
     SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI);
 
     display.init();
-    display.setTextColor(GxEPD_BLACK);
-
-    display.setRotation(0);
     display.fillScreen(GxEPD_WHITE);
-
-#if defined(_HAS_COLOR_)
-    display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_RED);
-#else
-    display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
-#endif
-
-#if defined(_HAS_SDCARD_)
-    display.setRotation(1);
-#if defined(LILYGO_T5_V102)
-    display.setCursor(5, display.height() - 15);
-#else
-    display.setCursor(20, display.height() - 15);
-#endif
-    String sizeString = "SD:" + String(SD.cardSize() / 1024.0 / 1024.0 / 1024.0) + "G";
-    display.println(rlst ? sizeString : "SD:N/A");
-
-    int16_t x1, x2;
-    uint16_t w, h;
-    String str = GxEPD_BitmapExamplesQ;
-    str = str.substring(2, str.lastIndexOf("/"));
-    display.getTextBounds(str, 0, 0, &x1, &x2, &w, &h);
-    display.setCursor(display.width() - w - 5, display.height() - 15);
-    display.println(str);
-#endif
-
     display.update();
 
-    delay(1000);
+    uint16_t serial0;
+    uint16_t serial1;
+    uint16_t serial2;
+    error = scd4x.getSerialNumber(serial0, serial1, serial2);
+    if (error)
+    {
+        Serial.print("Error trying to execute getSerialNumber(): ");
+        // errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
+    else
+    {
+        // printSerialNumber(serial0, serial1, serial2);
+        Serial.print("Serial: 0x");
+        // printUint16Hex(serial0);
+        // printUint16Hex(serial1);
+        // printUint16Hex(serial2);
+        Serial.println();
+    }
 
-    // Initialize the SCD4x sensor
-    scd4x.begin(Wire);
-    uint16_t error;
-    char errorMessage[256];
+    // Start Measurement
     error = scd4x.startPeriodicMeasurement();
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute startPeriodicMeasurement(): ");
-        errorToString(error, errorMessage, 256);
+        // errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     }
 
-    // Initialize the display
-    display.init();
-    display.setRotation(1);
-    display.setFont(&FreeMonoBold9pt7b);
-    display.setTextColor(GxEPD_BLACK);
+    Serial.println("Waiting for first measurement... (5 sec)");
 }
 
 void loop()
 {
-    drawCornerTest();
-
-    // int i = 0;
-    // while (i < 4) {
-    //     display.setRotation(i);
-    //     showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
-    //     //showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
-    //     //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
-    //     i++;
-    // }
-
-
-
-    //esp_sleep_enable_ext1_wakeup(((uint64_t)(((uint64_t)1) << BUTTON_1)), ESP_EXT1_WAKEUP_ALL_LOW);
-
-    //esp_deep_sleep_start();
-
     uint16_t co2;
     float temperature;
     float humidity;
@@ -203,13 +108,18 @@ void loop()
 
     // Read the CO2 level from the sensor
     error = scd4x.readMeasurement(co2, temperature, humidity);
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute readMeasurement(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else if (co2 == 0) {
+    }
+    else if (co2 == 0)
+    {
         Serial.println("Invalid sample detected, skipping.");
-    } else {
+    }
+    else
+    {
         Serial.print("CO2: ");
         Serial.print(co2);
         Serial.print(" ppm, Temperature: ");
@@ -218,57 +128,35 @@ void loop()
         Serial.print(humidity);
         Serial.println(" %RH");
 
-        // Display the CO2 level on the screen
-        display.fillScreen(GxEPD_WHITE);
+        // Display the CO2 level on the screen using partial update
+        display.setRotation(1);
+        display.setTextColor(GxEPD_BLACK);
+        display.setTextSize(2.5);
         display.setCursor(0, 30);
+        display.fillRect(0, 30, 250, 50, GxEPD_WHITE); // Clear the area where the CO2 value is displayed
         display.print("CO2: ");
         display.print(co2);
         display.println(" ppm");
-        //display.display();
+        display.updateWindow(0, 30, 250, 50, true); // Perform partial update on the specified area
     }
-    
-    display.fillScreen(GxEPD_WHITE);
-
-    display.update();
-
-    display.powerDown();
 
     delay(5000); // Wait for 5 seconds before the next reading
 }
 
-void showFont(const char name[], const GFXfont *f)
-{
-    display.fillScreen(GxEPD_WHITE);
-    display.setTextColor(GxEPD_BLACK);
-    display.setFont(f);
-    display.setCursor(0, 0);
-    display.println();
-    display.println(name);
-    display.println(" !\"#$%&'()*+,-./");
-    display.println("0123456789:;<=>?");
-    display.println("@ABCDEFGHIJKLMNO");
-    display.println("PQRSTUVWXYZ[\\]^_");
-    display.println("`abcdefghijklmno");
-    display.println("pqrstuvwxyz{|}~ ");
-    display.update();
-    delay(5000);
-}
-
-void drawCornerTest()
-{
-    display.drawCornerTest();
-    delay(5000);
-    uint8_t rotation = display.getRotation();
-    for (uint16_t r = 0; r < 4; r++) {
-        display.setRotation(r);
-        display.fillScreen(GxEPD_WHITE);
-        display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
-        display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
-        display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
-        display.fillRect(0, display.height() - 33, 32, 32, GxEPD_BLACK);
-        display.update();
-        delay(5000);
-    }
-    display.setRotation(rotation); // restore
-}
-
+// void showFont(const char name[], const GFXfont *f)
+// {
+//     display.fillScreen(GxEPD_WHITE);
+//     display.setTextColor(GxEPD_BLACK);
+//     display.setFont(f);
+//     display.setCursor(0, 0);
+//     display.println();
+//     display.println(name);
+//     display.println(" !\"#$%&'()*+,-./");
+//     display.println("0123456789:;<=>?");
+//     display.println("@ABCDEFGHIJKLMNO");
+//     display.println("PQRSTUVWXYZ[\\]^_");
+//     display.println("`abcdefghijklmno");
+//     display.println("pqrstuvwxyz{|}~ ");
+//     display.update();
+//     delay(5000);
+// }
